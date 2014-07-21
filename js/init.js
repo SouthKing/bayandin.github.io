06bd8ec25141c0fcd4ce634370d98cde65d2d633
Alexander Bayandin <a.bayandin@gmail.com>
2014-07-21 20:36:48 +0400

    SkelJS 0.4.9

--- js/init.js
+++ js/init.js
@@ -6,13 +6,14 @@
 
 // Skel.
 	skel.init({
-		reset: 'full',
+		resetCSS: true,
+		prefix: 'css/style',
+		grid: { range: '*', lockViewport: true, viewport: 'minimal-ui' },
 		breakpoints: {
-			'global': { range: '*', href: 'css/style.css', lockViewport: true, viewport: 'minimal-ui' },
-			'wide': { range: '-1680', href: 'css/style-wide.css' },
-			'normal': { range: '-1280', href: 'css/style-normal.css' },
-			'mobile': { range: '-640', href: 'css/style-mobile.css' },
-			'mobilep': { range: '-360', href: 'css/style-mobilep.css' }
+			'wide': { range: '-1680' },
+			'normal': { range: '-1280' },
+			'mobile': { range: '-640' },
+			'mobilep': { range: '-360' }
 		}
 	});
 
